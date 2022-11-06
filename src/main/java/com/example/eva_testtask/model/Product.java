b4afc19ef5706378e95dd5ad61f9ba415a789e37
Danylo Frolov <frolovdanilik@gmail.com>
2022-11-07 00:25:57 +0200

    working solution

---
+++ src/main/java/com/example/eva_testtask/model/Product.java
@@ -0,0 +1,61 @@
+package com.example.eva_testtask.model;
+
+import org.springframework.lang.NonNull;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    @Column(length = 100)
+    private String name;
+    @NonNull
+    @Column(length = 1000)
+    private String description;
+
+    public Product(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id
+                + ", name='" + name + '\''
+                + ", description='" + description + '\'' + '}';
+    }
+}
---
+++ src/main/java/com/example/eva_testtask/service/impl/ProductServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.eva_testtask.service.impl;
+
+import com.example.eva_testtask.dao.ProductDao;
+import com.example.eva_testtask.model.Product;
+import com.example.eva_testtask.service.ProductService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    private final ProductDao productDao;
+
+    public ProductServiceImpl(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return productDao.getAll();
+    }
+
+    @Override
+    public Product add(Product product) {
+        return productDao.add(product);
+    }
+
+    @Override
+    public Product getByName(String name) {
+        return productDao.getByName(name).orElseThrow(
+                () -> new RuntimeException("Can't get product by name " + name));
+    }
+}
